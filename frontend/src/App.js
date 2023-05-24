3b8903d27ce0b10eb626c0ee6e9c1a491e18a88b
Nicolas Yepes <nicolasyepespulido@gmail.com>
2023-05-23 23:47:39 -0500

    Created Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home  from "./Home"
-import ChessGame  from "./ChessGame"
-import io from 'socket.io-client'
-const socket = io.connect('http://localhost:4000')
+import Home  from "./pages/Home/Home";
+import ChessGame  from "./pages/ChessGame/ChessGame";
+import io from 'socket.io-client';
+import "./App.css";
+const socket = io.connect('http://localhost:4000');
 function App() {
   return (
-    <Router>
+    <Router class = "base">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/chess-room" element={<ChessGame/>}/>
