411d003323910adec44de3cdede878a33af2c786
Nicolas Yepes <nicolasyepespulido@gmail.com>
2023-05-23 19:14:04 -0500

    Started Backend with socket

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,21 @@
-const app = require('express')()
-const server = require('http').createServer(app)
-const socket_io = require('socket.io')(server)
+const express = require('express');
+const app = express();
+http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io')
 
-server.listen(4444, () => {
-    console.log("Server Started")
-})
+app.use(cors()); // Add cors middleware
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:4000',
+        methods: ['GET', 'POST']
+    }
+});
+io.on('connection', (socket) => {
+    console.log(`User Connected ${socket.id}`)
+});
+
+server.listen(4000, () => 'Server is running on port 3000');
